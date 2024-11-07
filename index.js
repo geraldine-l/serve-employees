287455e64408274d08f65dd951c263b58410c3c2
geraldine-l <geraldine.lonchay@gmail.com>
2024-11-07 21:33:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors({ origin: ["http://localhost:3000"] }));
+
+const sampleEmployee = {
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
+
+app.get("/api/employees", (req, res) => {
+  res.json({ results: [sampleEmployee] });
+});
+
+const port = 3310;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
